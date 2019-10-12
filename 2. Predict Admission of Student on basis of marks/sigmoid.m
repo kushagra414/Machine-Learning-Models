0de895b99a19d98d7a20dea5d55eb1648714a3dd
Kushagra Shekhawat <kushagrashekhawat414@gmail.com>
2019-10-13 00:25:46 +0530

    2. Predict Admission of Student on basis of marks

---
+++ 2. Predict Admission of Student on basis of marks/sigmoid.m
@@ -0,0 +1,16 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+
+% ======================  ======================
+% Computing the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+g = zeros(size(z));
+g = 1./(1+(e.^-z));
+
+
+% =============================================================
+
+end
